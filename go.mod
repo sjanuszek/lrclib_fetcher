b441990f0c0b3143c6f47c068899bdf290a82aee
sjanuszek <sjanuszek1@gmail.com>
2026-08-05 12:52:07 +0200

    moved from ffmpeg based solution of getting track duration to go native packages

--- go.mod
+++ go.mod
@@ -5,13 +5,18 @@ go 1.26.5
 require (
 	github.com/dhowden/tag v0.0.0-20240417053706-3d75831295e8
 	github.com/gdamore/tcell/v2 v2.13.10
+	github.com/hajimehoshi/go-mp3 v0.3.4
+	github.com/mewkiz/flac v1.0.13
 	github.com/rivo/tview v0.42.0
 	github.com/spf13/pflag v1.0.10
 )
 
 require (
 	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/icza/bitio v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
+	github.com/mewkiz/pkg v0.0.0-20250417130911-3f050ff8c56d // indirect
+	github.com/mewpkg/term v0.0.0-20241026122259-37a80af23985 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/term v0.37.0 // indirect
--- metadata/data_getter.go
+++ metadata/data_getter.go
@@ -2,11 +2,15 @@ package metadata
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/dhowden/tag"
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/mewkiz/flac"
 )
 
 type NecessaryData struct {
@@ -14,6 +18,56 @@ type NecessaryData struct {
 }
 
 func getTrackDuration(fileName string) (string, error) {
+	ext := filepath.Ext(fileName)
+
+	switch ext {
+		case ".flac":
+			return getFlacDuration(fileName)
+		case ".mp3":
+			return getMp3Duration(fileName)
+		default:
+			return getDurationFFprobe(fileName)
+	}
+}
+
+func getFlacDuration(fileName string) (string, error) {
+	stream, err := flac.ParseFile(fileName)
+	if err != nil {
+		return "", err
+	}
+
+	if stream.Info.SampleRate == 0 {
+		return "", fmt.Errorf("invalid sample rate")
+	}
+
+	duration := stream.Info.NSamples / uint64(stream.Info.SampleRate)
+	return fmt.Sprintf("%d", duration), nil
+}
+
+func getMp3Duration(fileName string) (string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	decoder, err := mp3.NewDecoder(file)
+	if err != nil {
+		return "", err
+	}
+
+	sampleRate := decoder.SampleRate()
+	if sampleRate == 0 {
+		return "", fmt.Errorf("invalid sample rate")
+	}
+
+	totalBytes := decoder.Length()
+	duration := totalBytes / int64(4*sampleRate)
+
+	return fmt.Sprintf("%d", duration), nil
+}
+
+func getDurationFFprobe(fileName string) (string, error) {
 	cmd := exec.Command("ffprobe", "-i", fileName, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0",)
 	var out bytes.Buffer
 	cmd.Stdout = &out
